4698ece976cf37c64798b4451c7f8fea3491290f
LKZsaldanha <38111193+LKZsaldanha@users.noreply.github.com>
2018-07-19 18:29:45 -0300

    Destruidor de objetos
    Destroi os objetos que saem da tela (por tag)

---
+++ 2DSimpleDodge/Assets/Scripts/GameSystem/ObjectsDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ObjectsDestroyer : MonoBehaviour
+{
+
+
+    public string[] destructableTags;
+
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        for (int i = 0; i < destructableTags.Length; i++)
+        {
+            if (collision.gameObject.CompareTag(destructableTags[i]))
+            {
+                Destroy(collision.gameObject);
+            }
+        }        
+    }
+}
