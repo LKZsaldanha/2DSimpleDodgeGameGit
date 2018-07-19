fa87e063a6ba4a3198f42f2c91dad590ef10a05c
LKZsaldanha <38111193+LKZsaldanha@users.noreply.github.com>
2018-07-19 18:30:53 -0300

    Sistema para criação de Waves usando prefabs

---
+++ 2DSimpleDodge/Assets/Scripts/GameSystem/WaveSetup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class WaveSetup : MonoBehaviour {
+
+    public Transform collectablePrefab;
+    public Transform[] collectableInstances;
+
+    public Transform harmfulPrefab;
+    public Transform[] harmfulInstances;
+
+    private void Awake()
+    {
+        InstantiateCollectables();
+        InstantiateHarmfull();
+        Destroy(gameObject);
+    }
+
+    private void InstantiateCollectables()
+    {
+        for (int i = 0; i < collectableInstances.Length; i++)
+        {
+            Instantiate(collectablePrefab, collectableInstances[i].position,Quaternion.identity);
+            Destroy(collectableInstances[i].gameObject);
+        }
+    }
+
+    private void InstantiateHarmfull()
+    {
+        for (int i = 0; i < harmfulInstances.Length; i++)
+        {
+            Instantiate(harmfulPrefab, harmfulInstances[i].position, Quaternion.identity);
+            Destroy(harmfulInstances[i].gameObject);
+        }
+    }
+}
