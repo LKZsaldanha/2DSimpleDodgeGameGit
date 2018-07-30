72076ee4370a682ace05563e9fe99c35f707a0a4
LKZsaldanha <38111193+LKZsaldanha@users.noreply.github.com>
2018-07-30 19:58:05 -0300

    Controle de UI

---
+++ 2DSimpleDodge/Assets/Scripts/GameSystem/UIController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    public GameObject startPanel;
+    public GameObject pausePanel;
+    public GameObject gamePanel;
+    public GameObject endPanel;
+
+    private GameManager gm;
+    private UIState currentUIState;
+
+    public Text highScoreLabel;
+    public Text endCurrentScoreLabel;
+
+    private void Start ()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    private void Update()
+    {
+        if(gm.uiState != currentUIState)
+        {
+            SetActiveUI();
+        }
+    }
+
+    private void UIUpdateInfo()
+    {
+        //setar visual dos botões de audio
+
+        highScoreLabel.text = gm.highScore.ToString();
+        endCurrentScoreLabel.text = GetComponent<Score>().score.ToString();
+    }
+
+    private void SetActiveUI()
+    {
+        startPanel.SetActive(false);
+        pausePanel.SetActive(false);
+        gamePanel.SetActive(false);
+        endPanel.SetActive(false);
+
+        switch (gm.uiState)
+        {
+            case UIState.START:
+                startPanel.SetActive(true);
+                break;
+            case UIState.PAUSE:
+                pausePanel.SetActive(true);
+                break;
+            case UIState.GAME:
+                gamePanel.SetActive(true);
+                break;
+            case UIState.END:
+                endPanel.SetActive(true);
+                break;
+        }
+        currentUIState = gm.uiState;
+        UIUpdateInfo();
+    }
+}
